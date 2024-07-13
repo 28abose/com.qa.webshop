4ad457d38af0b08bedc716cff109af3e83773324
bosed <bosed@Bose_ThinkPad.attlocal.net>
2024-07-13 15:21:25 -0700

    added extent report helper class and made changes in the base test

--- src/test/java/com/webshop/tests/LoginPageTests.java
+++ src/test/java/com/webshop/tests/LoginPageTests.java
@@ -7,37 +7,76 @@ public class LoginPageTests extends BaseTest {
 
 	@Test(priority = 1)
 	public void testLoginPageTitle() {
+		Logger = report.createTest("test login page title");
 		homePage.clickLoginLink();
+		Logger.info("clicked on login link");
 		String pageTitle = loginPage.getLoginPageTitle();
+		Logger.info("got the login page title");
 		Assert.assertTrue(pageTitle.contains("Login"));
+		Logger.pass("login page title matched");
 	}
 
 	@Test(priority = 2)
 	public void testWelcomeSignIn() {
+		Logger = report.createTest("test welcome sign in");
 		homePage.clickLoginLink();
+		Logger.info("clicked on login link");
 		String actual = loginPage.validateWelcomeText();
 		System.out.println(actual);
+		Logger.info("retrived the text from welcome sign in");
 		Assert.assertTrue(actual.contains("Welcome"));
+		Logger.pass("text contains welcome word");
 	}
 
 	@Test(priority = 3)
 	public void testForgotPasswordPasswordIsDisplayed() {
+		Logger = report.createTest("test forgot password link is displayed");
 		homePage.clickLoginLink();
+		Logger.info("clicked on login link");
 		boolean flag = loginPage.isForgotPasswordLinkPresent();
 		System.out.println(flag);
+		Logger.info("element forgot password link is located");
 		Assert.assertTrue(flag);
+		Logger.pass("forgot password link is displayed");
 	}
 
 	@Test(priority = 4)
 	public void testLoginFunction() {
+		Logger = report.createTest("test login functionality");
 		homePage.clickLoginLink();
+		Logger.info("clicked on login link");
 		loginPage.enterEmailId("selauto1@test.com");
+		Logger.info("entered email id");
 		loginPage.enterPassword("Pass@123");
+		Logger.info("entered password");
 		loginPage.clickLoginButton();
+		Logger.info("clicked on login button");
 		boolean flag = homePage.isLogoutLinkPresent();
 		System.out.println(flag);
+		Logger.info("located logout link");
 		Assert.assertTrue(flag);
+		Logger.pass("user able to successfully log in");
 		homePage.clickLogout();
+		Logger.info("clicked on logout");
 	}
 
+	@Test(priority = 5)
+	public void testLoginInvalidCredientials() {
+		Logger = report.createTest("test login functionality");
+		homePage.clickLoginLink();
+		Logger.info("clicked on login link");
+		loginPage.enterEmailId("selauto1@test.com");
+		Logger.info("entered email id");
+		loginPage.enterPassword("");
+		Logger.info("entered password");
+		loginPage.clickLoginButton();
+		Logger.info("clicked on login button");
+		boolean flag = homePage.isLogoutLinkPresent();
+		System.out.println(flag);
+		Logger.info("located logout link");
+		Assert.assertTrue(flag);
+		Logger.pass("user able to successfully log in");
+		homePage.clickLogout();
+		Logger.info("clicked on logout");
+	}
 }
