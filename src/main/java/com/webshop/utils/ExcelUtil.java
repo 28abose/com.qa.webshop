10191cf9dcc2b41e87553b1b8907ccaeebdbd72c
bosed <bosed@Bose_ThinkPad.attlocal.net>
2024-07-17 10:36:59 -0700

    Added Excel util code

--- src/main/java/com/webshop/utils/ExcelUtil.java
+++ src/main/java/com/webshop/utils/ExcelUtil.java
@@ -1,5 +1,36 @@
 package com.webshop.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ExcelUtil {
+
+	XSSFWorkbook excel;
+
+	// This constructor loads the excel sheet
+	public ExcelUtil(String path) {
+		try {
+			File src = new File(path);
+			FileInputStream fis = new FileInputStream(src);
+			excel = new XSSFWorkbook(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Unable to load the excel sheet :" + e.getLocalizedMessage());
+		}
+
+	}
+
+	// This method returns the active row count
+	public int getRowCount(int sheetNum) {
+		return excel.getSheetAt(sheetNum).getLastRowNum() + 1;
+	}
+
+	// This method returns the cell data
+	public String getCellData(int sheetnum, int row, int cell) {
+		return excel.getSheetAt(sheetnum).getRow(row).getCell(cell).toString();
+
+	}
 
 }
